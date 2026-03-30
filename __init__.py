13449acb226c864eee67b810f74db8fcbb03db7b
Paulo Vitor S. Soares <paulovitorsilvasoares@gmail.com>
2026-03-30 14:34:27 -0400

    Refactor project structure by moving quickdict_english module contents to root directory

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from .quickdict_english import main
+# -*- coding: utf-8 -*-
+# English Pop-up Dictionary for Anki
+
+from .reviewer import initialize_reviewer
+from .web import initialize_web
+
+initialize_web()
+initialize_reviewer()
--- quickdict_english/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# English Pop-up Dictionary for Anki
-
-from .reviewer import initialize_reviewer
-from .web import initialize_web
-
-initialize_web()
-initialize_reviewer()
